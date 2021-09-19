3447f44350c358ec32a31b1ff219d217bc116c22
chunu-j <codehouse9@naver.com>
2021-09-19 13:27:26 +0900

    외부톰캣 이용해보기 - jsp로 만들지 않아서 war 파일만으로 실행이 안됨

--- src/main/java/codehouse/simparty/SimPartyApplication.java
+++ src/main/java/codehouse/simparty/SimPartyApplication.java
@@ -2,20 +2,14 @@ package codehouse.simparty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class SimPartyApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SimPartyApplication.class);
-    }
+public class SimPartyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SimPartyApplication.class, args);
     }
+
 }
