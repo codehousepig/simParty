402eed76c08055e5361fd466a671625d7822de43
chunu-j <codehouse9@naver.com>
2021-10-02 20:10:11 +0900

    Update README + 조회 페이지 화면 구성

--- src/main/java/codehouse/simparty/dto/BookingDTO.java
+++ src/main/java/codehouse/simparty/dto/BookingDTO.java
@@ -6,7 +6,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 
 @Data
 @Builder
@@ -14,9 +17,61 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class BookingDTO {
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime startDate;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime endDate;
+
+    public boolean today() {
+        LocalDateTime today = LocalDateTime.now();
+        int a = today.getDayOfMonth();
+        int b = startDate.getDayOfMonth();
+        return a == b;
+    }
+
+    public boolean day1() {
+        LocalDateTime today = LocalDateTime.now();
+        int a = today.getDayOfMonth() + 1;
+        int b = startDate.getDayOfMonth();
+        return a == b;
+    }
+
+    public boolean day2() {
+        LocalDateTime today = LocalDateTime.now();
+        int a = today.getDayOfMonth() + 2;
+        int b = startDate.getDayOfMonth();
+        return a == b;
+    }
+
+    public boolean searchday() {
+
+        return true;
+    }
+
+    public int daystart() {
+        LocalTime start = LocalTime.of(11, 00);
+        return (int) ChronoUnit.MINUTES.between(start, startDate);
+    }
+    public int dayend() {
+        LocalTime start = LocalTime.of(11, 00);
+        int st = startDate.getDayOfMonth();
+        System.out.println("st = " + st);
+        int ed = endDate.getDayOfMonth();
+        System.out.println("ed = " + ed);
+
+        int num = ed - st;
+
+        System.out.println("aaaaaaaaaaaaaaaaa: " + ChronoUnit.DAYS.between(startDate, endDate));
+        System.out.println("daystart(): " + daystart());
+        int plus = 660 - daystart();
+        System.out.println("plus: " + plus);
+
+        if (num == 1) {
+            return (int) ChronoUnit.MINUTES.between(start, endDate) ;
+        } else {
+            return (int) ChronoUnit.MINUTES.between(start, endDate);
+        }
+    }
+
 }
